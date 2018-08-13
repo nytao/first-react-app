fe4d294731c41a2571ef983c7f0aa0b9dbff7743
Tao <nytaoxu@gmail.com>
2018-08-12 20:16:05 -0400

    First React App

---
+++ react.jsx
@@ -0,0 +1,42 @@
+"use strict";
+
+let e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: false
+    };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return e(
+        "button",
+        {
+          onClick: () => {
+            this.setState({
+              liked: false
+            })
+          }
+        },
+        "Dislike"
+      );
+    } else {
+      return e(
+        "button",
+        {
+          onClick: () => {
+            this.setState({
+              liked: true
+            });
+          }
+        },
+        "Like"
+      );
+    }
+  }
+}
+
+ReactDOM.render(e(LikeButton), document.getElementById("root"));
